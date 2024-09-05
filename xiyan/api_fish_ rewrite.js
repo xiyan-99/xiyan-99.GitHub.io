acf51007c0b6fd7f4341e974516d1975c2d376a3
xiyan <53385909+xiyan-99@users.noreply.github.com>
2024-09-05 16:13:40 +0800

    Update api_fish_ rewrite.js

--- xiyan/api_fish_ rewrite.js
+++ xiyan/api_fish_ rewrite.js
@@ -1,10 +1,2 @@
-const modifiedRequest = {
-  url: 'https://api.fish.audio/model',  // 目标请求 URL
-  headers: $request.headers
-};
-
-// 设置新的 Authorization 请求头
-modifiedRequest.headers['Authorization'] = 'Bearer ab5743267ba84d419a1b2a9a2ff02e98';
-
-// 发出修改后的请求
-$done({url: modifiedRequest.url, headers: modifiedRequest.headers});
+[rewrite_local]
+^https?:\/\/lf3-config\.bytetcc\.com\/obj\/tcc-config-web\/tcc-v2-data-lab\.speech\.tts_middle_layer-default url script-request-header file:///private/var/mobile/Library/Mobile%20Documents/iCloud~com~example~QuantumultX/Documents/api_fish_rewrite.js
