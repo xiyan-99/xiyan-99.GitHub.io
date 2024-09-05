7ca6afe8a6c73f7a0b474ed1daa875af51a15aff
xiyan-99 <1418581664@qq.com>
2024-09-05 15:59:07 +0800

    up

---
+++ Repo/Sileodepiction.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 定义通用 Sileodepiction 文件的 URL
+sileodepiction_url="https://raw.githubusercontent.com/xiyan-99/xiyan-99.GitHub.io/main/des/all.json"
+
+# 处理 Packages 文件，在每个包的 Description 行后添加 Sileodepiction 字段
+awk -v sileodepiction="$sileodepiction_url" '
+/^Description:/ {
+    print $0
+    print "Sileodepiction: " sileodepiction
+    next
+}
+{print}
+' Packages > Packages.new
+
+# 备份原 Packages 文件，并将新生成的文件替换原文件
+mv Packages Packages.bak
+mv Packages.new Packages
