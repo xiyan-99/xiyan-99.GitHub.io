3520b50fe89e3d091560f2df1f9b4810fa60dff6
xiyan-99 <1418581664@qq.com>
2025-01-15 22:49:30 +0800

    up

--- repo/repo/generate_packages.php
+++
@@ -1,6 +0,0 @@
-<?php
-$path = __DIR__ . '/debs';
-$cmd = "dpkg-scanpackages $path /dev/null > $path/../Packages && bzip2 -f $path/../Packages";
-exec($cmd);
-echo "Packages generated successfully.";
-?>
