6a182e8903de248efe4c5b4b5a5dd9e6264b7377
xiyan <53385909+xiyan-99@users.noreply.github.com>
2025-04-17 11:26:05 +0800

    Add files via upload

---
+++ QX/xy-themeboxqx.js
@@ -0,0 +1,5 @@
+[rewrite_local]
+^https?:\/\/theme\.25mao\.com\/index\/redeem url script-request-body https://raw.githubusercontent.com/xiyan-99/xiyan-99.GitHub.io/main/QX/xythemeboxqx.js
+
+[mitm]
+hostname = theme.25mao.com
