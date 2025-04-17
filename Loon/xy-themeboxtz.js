1928c74ace2013a089768edd91bada224d857bc1
xiyan <53385909+xiyan-99@users.noreply.github.com>
2025-04-17 11:41:09 +0800

    Add files via upload

---
+++ Loon/xy-themeboxtz.js
@@ -0,0 +1,13 @@
+#!name= 微信主题兑换通知 by 夕颜
+#!desc= 微信主题兑换通知 by 夕颜
+#!author= 夕颜
+#!homepage= https://cert.xiyan.store
+#!icon= https://img.xiyan.pro/i/u/2025/04/14/IMG_7887.png
+#!system = iOS
+#!system_version = 16.0
+#!loon_version = 3.2.1(372)
+#!tag = 主题,盒子,通知
+
+
+[script]
+http-request ^https:\/\/theme\.25mao\.com\/index\/redeem script-path=https://raw.githubusercontent.com/xiyan-99/xiyan-99.GitHub.io/main/Loon/xythemeboxtz.js, requires-body=true, timeout=20, tag=兑换主题通知,
