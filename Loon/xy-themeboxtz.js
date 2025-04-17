b3bb659211e284594f1a6ea54f9d55434bf0be27
xiyan <53385909+xiyan-99@users.noreply.github.com>
2025-04-17 11:44:10 +0800

    Update xy-themeboxtz.js

--- Loon/xy-themeboxtz.js
+++ Loon/xy-themeboxtz.js
@@ -1,5 +1,5 @@
-#!name= 微信主题兑换通知 by 夕颜
-#!desc= 微信主题兑换通知 by 夕颜
+#!name= 盒子兑换通知 by 夕颜
+#!desc= 微信主题兑换通知 by 夕颜-loon插件
 #!author= 夕颜
 #!homepage= https://cert.xiyan.store
 #!icon= https://img.xiyan.pro/i/u/2025/04/14/IMG_7887.png
@@ -8,6 +8,9 @@
 #!loon_version = 3.2.1(372)
 #!tag = 主题,盒子,通知
 
-
 [script]
 http-request ^https:\/\/theme\.25mao\.com\/index\/redeem script-path=https://raw.githubusercontent.com/xiyan-99/xiyan-99.GitHub.io/main/Loon/xythemeboxtz.js, requires-body=true, timeout=20, tag=兑换主题通知,
+
+
+[mitm]
+hostname = theme.25mao.com
